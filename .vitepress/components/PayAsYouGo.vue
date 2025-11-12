99a43fe404510486d8fe3537804498234931a21c
mihail-izumov <106297169+mihail-izumov@users.noreply.github.com>
2025-11-12 19:41:07 +0400

    Update PayAsYouGo.vue

--- .vitepress/components/PayAsYouGo.vue
+++ .vitepress/components/PayAsYouGo.vue
@@ -27,7 +27,7 @@ const goToApply = () => {
       Наше обязательство
     </button>
     <button :class="{ 'mr-active': activeTab === 'guarantee-details' }" @click="activeTab = 'guarantee-details'">
-      Как это работает
+      Как работает
     </button>
     <button :class="{ 'mr-active': activeTab === 'cause' }" @click="activeTab = 'cause'">
       Что значит «по нашей вине»
@@ -36,7 +36,7 @@ const goToApply = () => {
       Сравнение с рынком
     </button>
     <button :class="{ 'mr-active': activeTab === 'for-whom' }" @click="activeTab = 'for-whom'">
-      Для кого это
+      Для кого
     </button>
     <button :class="{ 'mr-active': activeTab === 'how-to-start' }" @click="activeTab = 'how-to-start'">
       Как начать
@@ -71,7 +71,7 @@ const goToApply = () => {
           <svg :class="{ 'mr-open': openAccordions.commitment1 }" class="mr-chevron" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
             <path d="m6 9 6 6 6-6"/>
           </svg>
-          <span class="mr-acc-title">Карту системных узких мест</span>
+          <span class="mr-acc-title">Карта системных узких мест</span>
         </button>
         <div v-show="openAccordions.commitment1" class="mr-accordion-body">
           <ul class="mr-ul">
@@ -199,7 +199,7 @@ const goToApply = () => {
           </tr>
           <tr>
             <td>Когда результат</td>
-            <td>Через 6-12 месяцев</td>
+            <td>Через 3-6 месяцев</td>
             <td>Через 30 дней</td>
           </tr>
           <tr>
