29ea6e7c0f76b78ef9bd2ff60bd0b231fab390dc
mihail-izumov <106297169+mihail-izumov@users.noreply.github.com>
2025-11-12 15:46:09 +0400

    Update CheckupHowWorks.vue

--- .vitepress/components/CheckupHowWorks.vue
+++ .vitepress/components/CheckupHowWorks.vue
@@ -17,7 +17,7 @@
           href="/apply"
           class="card-link-custom"
         >
-          Подать заявку на Чекап
+          Подать заявку <br>на Чекап
           <span class="arrow-icon-custom" aria-hidden="true">
             <svg width="18" height="18" viewBox="0 0 24 24" fill="none"
               stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
@@ -60,7 +60,7 @@ const cards = [
 ];
 
 // Формируем автоматическую подстановку названия месяца для первой карточки
-const descriptionWithMonth = `Не гадаем по симптомам, а получаем объективную картину всех 10 разделов бизнеса за месяц. Результаты Чекапа в ${nowMonth}.`;
+const descriptionWithMonth = `Результаты Чекапа в ${nowMonth}. Не гадаем по симптомам, а получаем объективную картину всех 10 разделов бизнеса за месяц.`;
 </script>
 
 <style scoped>
