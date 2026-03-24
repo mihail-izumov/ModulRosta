ba9d6fcebc182647d2b47ee008fb5b9e8c0ae75a
mihail-izumov <106297169+mihail-izumov@users.noreply.github.com>
2026-03-24 14:40:56 +0400

    Create LaunchesPage.vue

---
+++ .vitepress/launches/LaunchesPage.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="mr-page">
+    <MRHistory />
+    <LaunchesChart />
+  </div>
+</template>
+
+<script setup lang="ts">
+import MRHistory from './MRHistory.vue'
+import LaunchesChart from './LaunchesChart.vue'
+</script>
